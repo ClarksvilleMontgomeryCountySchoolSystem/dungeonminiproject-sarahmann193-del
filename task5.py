2f1f6b85550f231f83723afe5446c18fab39c282
sarahmann193-del <sarah.mann193@students.cmcss.net>
2026-03-03 13:31:08 -0600

    Update task5.py

---
+++ task5.py
@@ -0,0 +1,6 @@
+escaped = True
+if escaped:
+    outcome = "Yay! You escaped the dungeon and lived a long happy life."
+else:
+    outcome = "You wake up from your dream...still in the dungeon."
+print(outcome)
