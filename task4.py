c48ad7e62fb45b33e41fad12e8ff0eb6da378e06
sarahmann193-del <sarah.mann193@students.cmcss.net>
2026-03-03 13:30:50 -0600

    Update task4.py

---
+++ task4.py
@@ -0,0 +1,6 @@
+drawbridge_raised = False
+if not drawbridge_raised:
+    outcome = "Thunder: Awesome! You quickly passed the drawbridge and escaped the dungeon!"
+else:
+    outcome = "Doom: You try to swim across the moat...but the water is infested with hungry crocodiles."
+print(outcome)
