c92f6303dc02aa8e02e0990d97eede6275c39f3e
sarahmann193-del <sarah.mann193@students.cmcss.net>
2026-03-03 13:29:57 -0600

    Update task1.py

---
+++ task1.py
@@ -0,0 +1,6 @@
+torch_lit = True
+if torch_lit:
+    outcome = "Flicker: you find a hallway and see a door."
+else:
+    outcome = "Doom: you cannot see and hit your head. You pass out."
+print(outcome)
