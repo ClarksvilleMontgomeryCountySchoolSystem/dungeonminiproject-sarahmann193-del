594888be8b3dc09694169fe6416152938d2eba34
sarahmann193-del <sarah.mann193@students.cmcss.net>
2026-03-03 13:30:31 -0600

    Update task3.py

---
+++ task3.py
@@ -0,0 +1,6 @@
+guard_awake = False
+if not guard_awake:
+    outcome = "Shadow: You are able to sneak past him and out of the dungeon."
+else:
+    outcome = "Doom: The guard sees you and chases you back to the dungeon."
+print(outcome)
